7fd82b52345258022b13aac47dc99030c162ae71
jtheol <130566002+jtheol@users.noreply.github.com>
2023-08-10 21:02:57 -0400

    Created visualizations and cleaned text data.

--- src/data/build_dataset.py
+++ src/data/build_dataset.py
@@ -1,10 +1,13 @@
 import os
 import re
+import string
 
 import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
+
+from nltk.corpus import stopwords
 
 pd.options.display.float_format = "{:2f}".format
 
@@ -19,6 +22,7 @@ cancellations = pd.read_excel(
 shipments.info()
 shipments[["Quantity", "Acquisition Value"]].describe()
 shipments.isnull().sum()
+shipments
 
 cancellations.info()
 cancellations[["Quantity", "Acquisition Value"]].describe()
@@ -26,6 +30,45 @@ cancellations.isnull().sum()
 cancellations.dropna(axis=0, how="any", inplace=True)
 cancellations.reset_index(drop=True)
 
+
+# ――――――――――――――――――――――――――――――――――――――――――――――
+# CLEANING JUSTIFICATIONS AND REASONS CANCELLED
+# ――――――――――――――――――――――――――――――――――――――――――――――
+def clean_text(text: pd.Series) -> pd.Series:
+    """Used to clean text for both shipment justifications and cancellations.
+
+    Args:
+        text (pd.Series): Column from dataframe containing text to be cleaned.
+
+    Returns:
+        pd.Series: Resulting column after text has been cleaned.
+    """
+
+    text = text.str.lower()
+    text = text.str.translate(str.maketrans("", "", string.punctuation))
+
+    # Cleaning stop words
+    stop_words = set(stopwords.words("english"))
+    text = text.apply(
+        lambda x: " ".join(w for w in x.split(" ") if w not in stop_words)
+    )
+
+    # Removing numbers
+    text = text.str.replace(r"\d+", "", regex=True)
+
+    text = text.str.replace("\n", "")
+
+    return text
+
+
+# Shipments Justifications (shipments)
+shipments["cleaned_justification"] = clean_text(shipments["Justification"])
+
+# Reasons Cancelled (cancellations)
+cancellations["cleaned_justification"] = clean_text(cancellations["Justification"])
+cancellations["cleaned_reason_cancelled"] = clean_text(
+    cancellations["Reason Cancelled"]
+)
 
 shipments.to_csv("../../data/processed/proc-shipments.csv", index=False)
 cancellations.to_csv("../../data/processed/proc-cancellations.csv", index=False)
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,5 +2,54 @@ import os
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use("ggplot")
+
+
+shipments = pd.read_csv("../../data/processed/proc-shipments.csv")
+
+cancellations = pd.read_csv("../../data/processed/proc-cancellations.csv")
+
+# ――――――――――――――――――――――
+# VISUALIZING SHIPMENTS
+# ――――――――――――――――――――――
+# Shipments by State
+pal_blue_reversed_ship = sns.color_palette(
+    "Blues", n_colors=len(shipments["State"].unique())
+)
+pal_blue_reversed_ship.reverse()
+sns.barplot(
+    x=shipments["State"].value_counts().values,
+    y=shipments["State"].value_counts().index,
+    palette=pal_blue_reversed_ship,
+)
+plt.title("Number of Shipments by State")
+plt.savefig("../visualization/figures/shipments-by-state.png")
+
+# Shipments over time
+shipments_time = shipments[["Date Shipped", "Acquisition Value"]].sort_values(
+    by="Date Shipped"
+)
+sns.lineplot(
+    data=shipments_time,
+    x="Date Shipped",
+    y="Acquisition Value",
+    color="#83ceff",
+    markeredgecolor="black",
+    markeredgewidth=1,
+    marker="o",
+    markersize=6,
+    linewidth=2,
+)
+plt.xticks(rotation=90)
+plt.tick_params(axis="x", which="major", labelsize=5)
+plt.title("Acquisition Value April - June 2023")
+plt.tight_layout()
+plt.show()
+
+# ――――――――――――――――――――――――――
+# VISUALIZING CANCELLATIONS
+# ――――――――――――――――――――――――――
